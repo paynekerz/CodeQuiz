e337cfb15fb9351d9f7d74dbd085956c1202b38b
Payne Kerz <paynekerz@gmail.com>
2021-06-02 18:30:04 -0700

    first commit

---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+const startBtn = document.getElementById("start-btn")
+startBtn.addEventListener("click", startQuiz)
+
+function quizTimer() {
+    
+}
+
+function startQuiz() {
+    quizTimer();
+}
+
+function nextQuestion() {
+
+}
+
